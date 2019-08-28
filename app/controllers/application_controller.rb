a7b788b36e8d54e3821f3a0b19695581451674f2
Manish Shrestha <manish@proshore.eu>
2019-08-28 19:39:53 +0545

    Changes error message for invalid jwt token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
         rescue ActiveRecord::RecordNotFound => e
           render json: { errors: e.message }, status: :not_found
         rescue JWT::DecodeError => e
-          render json: { errors: e.message }, status: :unauthorized
+          render json: { errors: "Invalid access token." }, status: :unauthorized
         end
       end
 end
