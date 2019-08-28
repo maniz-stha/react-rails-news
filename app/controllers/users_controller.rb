6967069336e9e989894083ef1187b83b6c311582
Manish Shrestha <manish@proshore.eu>
2019-08-28 19:35:22 +0545

    Implements jwt for authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
     skip_before_action :verify_authenticity_token
-
+    before_action :authorize_request, only: [:show] 
+    
     def index
-        render plain: 'Hello'
+        @users = User.all
+        render json: {status: "SUCCESS",  message: "Users listing", data: @users}, status: :ok
     end
 
     def register
@@ -21,12 +23,16 @@ class UsersController < ApplicationController
 
     #login using username of email
     def login
+        # render json: login_params.inspect
         params = login_params
         user = User.find_by("email = ? OR username = ?", params[:identity].downcase, params[:identity])
         if user
             # user found, authenticate
             if user.authenticate(params[:password])
-                user_data = {id: user.id, username: user.username, name: user.name, email: user.email}
+                #set expiry to 2 hours
+                time = Time.now + 2.hours.to_i
+                token = JsonWebToken.encode({user_id: user.id}, time)
+                user_data = {id: user.id, username: user.username, name: user.name, email: user.email, token: token, exp: time.strftime("%m-%d-%Y %H:%M")}
                 render json: {status: "SUCCESS", message: "User logged in", data: user_data}, status: :ok
             else
                 render json: {password: "Incorrect password"}, status: :bad_request
