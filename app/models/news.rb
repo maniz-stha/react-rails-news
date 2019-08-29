fc88ed4ba4c43375e60f329be6e9d4d183004084
Manish Shrestha <manish@proshore.eu>
2019-08-30 05:42:05 +0545

    Add url validation for the link

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,7 +1,7 @@
 class News < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
-  validates :link, presence: true
+  validates :link, presence: true, format: /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i
   validates :source, presence: true
   validates :user, presence: true
 end
