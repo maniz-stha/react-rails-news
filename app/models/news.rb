f76a7d3558646b6b74f51be6aeabb3d9a1d15a67
Manish Shrestha <manish@proshore.eu>
2019-08-29 22:51:53 +0545

    Creates basic news CRUD

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,3 +1,7 @@
 class News < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true
+  validates :link, presence: true
+  validates :source, presence: true
+  validates :user, presence: true
 end
