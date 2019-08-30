9a631d3e8d51c76f17257a6a94e71451801f149f
Manish Shrestha <manish@proshore.eu>
2019-08-30 19:16:45 +0545

    Redux setup including basic components for login and register routes

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import './App.css';
+import store from './store';
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
 
 function App() {
   return (
-    <div className="App">
-      <h1>News room</h1>
-    </div>
+    <Provider store={ store }>
+      <Router>
+        <div className="App">
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
