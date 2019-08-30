1ee07ea4baa89469d666cdf65e4e6bd5b89757bb
Manish Shrestha <manish@proshore.eu>
2019-08-30 23:16:38 +0545

    Login & register forms

--- app/frontend/src/components/auth/Login.js
+++ app/frontend/src/components/auth/Login.js
@@ -3,8 +3,22 @@ import React, { Component } from 'react'
 class Login extends Component {
     render() {
         return (
-            <div>
-                Login page
+            <div className="login row">
+                <div className="col-md-8 m-auto">
+                    <h2 className="text-center">Log In</h2>
+                    <form>
+                        <div class="form-group">
+                            <label for="identity">Email Address / Username</label>
+                            <input type="text" class="form-control" name="identity" id="identity" aria-describedby="identityHelp" placeholder="Enter email or username"/>
+                            <small id="identityHelp" class="form-text text-muted">Use your email address or username to login</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" name="password" id="password" placeholder="Password"/>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
             </div>
         )
     }
--- app/frontend/src/components/auth/Register.js
+++ app/frontend/src/components/auth/Register.js
@@ -3,8 +3,33 @@ import React, { Component } from 'react'
 class Register extends Component {
     render() {
         return (
-            <div>
-                Register page
+            <div className="register row">
+                <div className="col-md-8 m-auto">
+                    <h2 className="text-center">Register</h2>
+                    <form>
+                        <div class="form-group">
+                            <label for="username">Username</label>
+                            <input type="text" class="form-control" name="username" id="username" placeholder="Enter username"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" class="form-control" name="name" id="name" placeholder="Enter full name"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" name="email" id="email" placeholder="Enter email"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" name="password" id="password" placeholder="Password"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="password_confirmation">Confirm Password</label>
+                            <input type="password_confirmation" class="form-control" name="password_confirmation" id="password_confirmation" placeholder="Confirm Password"/>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
             </div>
         )
     }
