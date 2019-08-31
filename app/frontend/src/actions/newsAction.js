cfae0bc300169303c25bcf8519899d0da84e2350
Manish Shrestha <manish@proshore.eu>
2019-08-31 17:48:14 +0545

    Design updates on news item component

--- app/frontend/src/actions/newsAction.js
+++ app/frontend/src/actions/newsAction.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GET_ERRORS, ADD_NEWS } from './types';
+import { GET_ERRORS } from './types';
 import { SET_NEWS_LIST } from './types';
 
 export const getNewsList = () => dispatch => {
--- app/frontend/src/components/news/NewsItem.js
+++ app/frontend/src/components/news/NewsItem.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Moment from 'react-moment';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class NewsItem extends Component {
+
+    onLikeClick() {
+        //TODO: like on news
+        alert('liked');
+    }
+
+    onDeleteClick() {
+        //TODO: delete news
+        alert('delete');
+    }
+
     render() {
-        const { news } = this.props;
+        const { news, auth } = this.props;
         return (
             <div className="card card-body bg-light mb-3">
                 <div className="news-title">
@@ -12,10 +25,27 @@ class NewsItem extends Component {
                     <span className="news-source ml-3">({news.source})</span>
                 </div>
                 <div className="news-info">
-                    <span className="news-like mr-3">Likes</span>
-                    <span className="news-coment mr-3">Comments</span>
-                    <span className="news-poster mr-3">Posted by user ({news.user_id})</span> at  
-                    <span>{news.updated_at}</span>
+                    <a href="#" className="news-like news-action" onClick={this.onLikeClick.bind(this)}>
+                        <i className="fas fa-thumbs-up" />
+                        <span className="badge badge-light">1</span>
+                    </a>
+                    <Link to={`/news/${news.id}`} className="news-comment news-action mx-5">Comment</Link>
+                    <span className="news-poster mr-3">
+                        Posted by user ({news.user_id}) <Moment fromNow>{news.updated_at}</Moment>
+                    </span>
+                    {
+                        // if posted by same user, allow edit and delete
+                        news.user_id === auth.user.id ? (
+                            <span className="actions float-right">
+                                <Link className="news-edit news-action mr-2" to={`/news/edit/${news.id}`}>
+                                    <i className="fas fa-edit" />
+                                </Link>
+                                <a href="#" onClick={this.onDeleteClick.bind(this, news.id)} className="news-delete news-action">
+                                    <i className="fas fa-times" />
+                                </a>
+                            </span>
+                        ): null
+                    }
                 </div>
             </div>
         )
@@ -23,7 +53,12 @@ class NewsItem extends Component {
 }
 
 NewsItem.propTypes = {
-  news: PropTypes.object.isRequired
+    news: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired
 };
 
-export default NewsItem;
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps)(NewsItem);
